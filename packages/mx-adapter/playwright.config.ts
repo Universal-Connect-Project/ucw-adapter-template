a68f3d3d0409983e906b115a7a1d71c5e14d5bd2
Wes Risenmay <wes.risenmay@mx.com>
2024-11-21 10:17:52 -0700

    trying to fix playwright tests

--- packages/mx-adapter/playwright.config.ts
+++ packages/mx-adapter/playwright.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
   ],
   webServer: {
     command: "npm run dev:e2e",
+    cwd: "../../",
     url: "http://127.0.0.1:8080",
     reuseExistingServer: !process.env.CI,
   },
