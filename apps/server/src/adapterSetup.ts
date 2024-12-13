d05102f6c0a43faf580d3a5e05ad7e144eb0d1af
Craig Tingey <craig.tingey@mx.com>
2024-12-13 14:37:20 -0700

    Remove adapters from adapterSetup

--- apps/server/src/adapterSetup.ts
+++ apps/server/src/adapterSetup.ts
@@ -1,55 +1,13 @@
 import type { AdapterMap } from "@repo/utils";
-import { getMxAdapterMapObject } from "@ucp-npm/mx-adapter";
-import { getSophtronAdapterMapObject } from "@ucp-npm/sophtron-adapter";
 import { getTemplateAdapterMapObject } from "@ucp-npm/template-adapter";
-import config from "./config";
-import * as logger from "./infra/logger";
-import { get, set } from "./services/storageClient/redis";
 import { adapterMapObject as testAdapterMapObject } from "./test-adapter";
 
 const templateAdapterMapObject = getTemplateAdapterMapObject();
 
-const mxAdapterMapObject = getMxAdapterMapObject({
-  cacheClient: {
-    set: set,
-    get: get,
-  },
-  logClient: logger,
-  aggregatorCredentials: {
-    mxInt: {
-      username: config.MX_CLIENT_ID,
-      password: config.MX_API_SECRET,
-    },
-    mxProd: {
-      username: config.MX_CLIENT_ID_PROD,
-      password: config.MX_API_SECRET_PROD,
-    },
-  },
-  envConfig: {
-    HOSTURL: config.HOST_URL,
-    PROXY_HOST: config.PROXY_HOST,
-    PROXY_PORT: config.PROXY_PORT,
-    PROXY_USERNAME: config.PROXY_USERNAME,
-    PROXY_PASSWORD: config.PROXY_PASSWORD,
-  },
-});
-
-const sophtronAdapterMapObject: Record<string, AdapterMap> =
-  getSophtronAdapterMapObject({
-    logClient: logger,
-    aggregatorCredentials: {
-      clientId: config.SOPHTRON_API_USER_ID,
-      secret: config.SOPHTRON_API_USER_SECRET,
-    },
-    envConfig: {
-      HOSTURL: config.HOST_URL,
-    },
-  });
-
+// This is where you add adapters
 export const adapterMap: Record<string, AdapterMap> = {
   ...templateAdapterMapObject,
   ...testAdapterMapObject,
 };
-
 export type Aggregator = keyof typeof adapterMap;
 export const aggregators = Object.keys(adapterMap);
